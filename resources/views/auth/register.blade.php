cd50768759c7d5014ec7a81da18c4b1146707bd0
Kevin Vivas <kvinloaiza93@gmail.com>
2024-04-04 18:55:31 -0500

    Registro_2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,13 @@
+@php
+    $municipios = App\Models\Municipio::all();
+    $identificaciones = App\Models\TipoIdentificacion::all();
+    $generos = App\Enums\Genero::getValues();
+@endphp
+
+
+
 <x-guest-layout>
+
     <script src="https://cdn.tailwindcss.com"></script>
     <form class="bg-amber-200 text-black" method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
         @csrf
@@ -6,36 +15,36 @@
         <!-- Primer nombre -->
         <div>
             <x-input-label for="first-name" :value="__('Primer nombre')" />
-            <x-text-input id="first-name" class="block mt-1 w-full" type="text" name="first-name" :value="old('first-name')"
-                required autofocus autocomplete="first-name" />
-            <x-input-error :messages="$errors->get('firt-name')" class="mt-2" />
+            <x-text-input id="first-name" class="block mt-1 w-full" type="text" name="primer_nombre" :value="old('first-name')"
+                required autofocus autocomplete="primer_nombre" />
+            <x-input-error :messages="$errors->get('primer_nombre')" class="mt-2" />
         </div>
 
         <!-- Segundo nombre -->
 
         <div class="mt-4">
             <x-input-label for="second-name" :value="__('Segundo nombre')" />
-            <x-text-input id="second-name" class="block mt-1 w-full" type="text" name="second-name" :value="old('second-name')"
-                autofocus autocomplete="second-name" />
-            <x-input-error :messages="$errors->get('last-name')" class="mt-2" />
+            <x-text-input id="second-name" class="block mt-1 w-full" type="text" name="segundo_nombre" :value="old('second-name')"
+                autofocus autocomplete="segundo_nombre" />
+            <x-input-error :messages="$errors->get('segundo_nombre')" class="mt-2" />
         </div>
 
         <!-- Primer apellido -->
 
         <div class="mt-4">
             <x-input-label for="first-surname" :value="__('Primer apellido')" />
-            <x-text-input id="first-surname" class="block mt-1 w-full" type="text" name="first-surname"
-                :value="old('first-surname')" required autofocus autocomplete="first-surname" />
-            <x-input-error :messages="$errors->get('first-surname')" class="mt-2" />
+            <x-text-input id="first-surname" class="block mt-1 w-full" type="text" name="primer_apellido"
+                :value="old('first-surname')" required autofocus autocomplete="primer_apellido" />
+            <x-input-error :messages="$errors->get('primer_apellido')" class="mt-2" />
         </div>
 
         <!-- Segundo apellido -->
 
         <div class="mt-4">
             <x-input-label for="second-surname" :value="__('Segundo apellido')" />
-            <x-text-input id="second-surname" class="block mt-1 w-full" type="text" name="second-surname"
-                :value="old('second-surname')" autofocus autocomplete="second-surname" />
-            <x-input-error :messages="$errors->get('second-surname')" class="mt-2" />
+            <x-text-input id="second-surname" class="block mt-1 w-full" type="text" name="segundo_apellido"
+                :value="old('second-surname')" autofocus autocomplete="segundo_apellido" />
+            <x-input-error :messages="$errors->get('segundo_apellido')" class="mt-2" />
         </div>
 
         <!-- Género -->
@@ -43,9 +52,14 @@
         <div class="mt-4">
             <x-input-label for="genero" :value="__('Género')" />
             <select name="" id="genero" class="rounded-md mt-1 w-full " required>
-                <option value="" class="bg-amber-300">------------Seleccione una opción------------</option>
-                <option value="" class="bg-amber-300">Maculino</option>
-                <option value="" class="bg-amber-300">Femenino</option>
+                <option value="" class="bg-amber-300">------------Seleccione un genero------------</option>
+
+                @foreach ($generos as $genero)
+                    <option value="{{ $genero }}" class="bg-amber-300">
+                        {{ $genero }}
+                    </option>
+                @endforeach
+
             </select>
         </div>
 
@@ -67,15 +81,13 @@
 
         <div class="mt-4">
             <x-input-label for="Tipoidentficacion" :value="__('Tipo de Identficación')" />
-            <select name="" id="genero" class="rounded-md mt-1 w-full " required>
+            <select name="tipo_identificacions_id" id="genero" class="rounded-md mt-1 w-full " required>
                 <option value="" class="bg-amber-300">------------Seleccione una opción------------</option>
-                <option value="" class="bg-amber-300">(CC) Cedula de Ciudadanía</option>
-                <option value="" class="bg-amber-300">(CE) Cédula de extrangeria</option>
-                <option value="" class="bg-amber-300">(PP) Pasaporte</option>
-                <option value="" class="bg-amber-300">(TI) Tarjeta de Identidad</option>
-                <option value="" class="bg-amber-300">(RCN) Registro Civil de Nacimiento</option>
-                <option value="" class="bg-amber-300">(TRE) Tarjeta de Registro de Extranjero</option>
-                <option value="" class="bg-amber-300">(PEP) Permiso Especial de Permanencia</option>
+                @foreach ($identificaciones as $identificacion)
+                    <option value="{{ $identificacion->id }}" class="bg-amber-300">
+                        {{ $identificacion->nombre_identificacion }}
+                    </option>
+                @endforeach
             </select>
         </div>
 
@@ -84,37 +96,41 @@
         <!-- Identificación -->
 
         <div class="mt-4">
-            <x-input-label for="identficacion" :value="__('Identficación')" />
-            <x-text-input id="identficacion" class="block mt-1 w-full" type="text" name="identficacion"
-                :value="old('identficacion')" required autofocus autocomplete="identficacion" />
-            <x-input-error :messages="$errors->get('identficacion')" class="mt-2" />
+            <x-input-label for="identificacion" :value="__('Identificación')" />
+            <x-text-input id="identificacion" class="block mt-1 w-full" type="text" name="numero_identficacion"
+                :value="old('identificacion')" required autofocus autocomplete="numero_identficacion" />
+            <x-input-error :messages="$errors->get('numero_identficacion')" class="mt-2" />
         </div>
 
-        <!-- Tipo de Identificación -->
 
-        <div class="mt-4">
-            <x-input-label for="muni_residencia" :value="__('Municipio de Residencia')" />
-            <select name="" id="muni_residencia" class="rounded-md mt-1 w-full " required>
-                <option value="" class="bg-amber-300">------------Seleccione una opción------------</option>
-            </select>
-        </div>
 
-        <!-- Tipo de Identificación -->
+        <!-- Municipio Nacimiento -->
 
         <div class="mt-4">
             <x-input-label for="muni_nacimiento" :value="__('Municipio de Nacimiento')" />
-            <select name="" id="muni_nacimiento" class="rounded-md mt-1 w-full " required>
+            <select name="municipio_nacimiento_id" id="muni_nacimiento" class="rounded-md mt-1 w-full " required>
                 <option value="" class="bg-amber-300">------------Seleccione una opción------------</option>
+
+                @foreach ($municipios as $municipio)
+                    <option value="{{ $municipio->id }}" class="bg-amber-300">{{ $municipio->nombre_municipio }}
+                    </option>
+                @endforeach
+
             </select>
         </div>
 
 
-        <!-- Tipo de Identificación -->
+        <!-- Municipio Residencia -->
 
         <div class="mt-4">
             <x-input-label for="muni_residencia" :value="__('Municipio de Residencia')" />
-            <select name="" id="muni_residencia" class="rounded-md mt-1 w-full " required>
+            <select name="municipio_residencia_id" id="muni_residencia" class="rounded-md mt-1 w-full " required>
                 <option value="" class="bg-amber-300">------------Seleccione una opción------------</option>
+
+                @foreach ($municipios as $municipio)
+                    <option value="{{ $municipio->id }}" class="bg-amber-300">{{ $municipio->nombre_municipio }}
+                    </option>
+                @endforeach
             </select>
         </div>
 
@@ -128,29 +144,27 @@
             <x-input-error :messages="$errors->get('foto')" class="mt-2" />
         </div> --}}
 
-        <div class="mt-4">
+        {{-- <div class="mt-4">
             <x-input-label for="foto" :value="__('Foto de perfil')" />
             <input id="foto" class="block mt-1 w-full bg-amber-400 border-2 border-white" type="file"
                 name="foto" required autofocus autocomplete="foto" />
             <x-input-error :messages="$errors->get('foto')" class="mt-2" />
-        </div>
+        </div> --}}
 
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Correo Electrónico')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                required autocomplete="username" />
+                required autocomplete="email" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Contraseña')" />
-
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
                 autocomplete="new-password" />
-
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
